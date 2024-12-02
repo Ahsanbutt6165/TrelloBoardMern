33fca2de93851587465858ce31f4cc60f529f3ee
Ahsanbutt6165 <166932664+Ahsanbutt6165@users.noreply.github.com>
2024-12-02 17:06:28 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import boardRoutes from "./routes/boardRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import UserRoutes from "./routes/UserRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import connectToMongo from "./database/db.js";
-import CardRoutes from "./routes/CardRoutes.js";
+import cardRoutes from "./routes/cardRoutes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
@@ -22,16 +22,16 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 // Routes
-app.use("/api/user", UserRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/board", boardRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/card", CardRoutes);
+app.use("/api/card", cardRoutes);
 
-// const __dirname = path.resolve("/Users/hp/Desktop/New folder");
-// app.use(express.static(path.join(__dirname, "/vite-project/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "vite-project", "dist", "index.html"));
-// });
+const __dirname = path.resolve("/Users/hp/Desktop/New folder");
+app.use(express.static(path.join(__dirname, "/vite-project/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "vite-project", "dist", "index.html"));
+});
 
 // Start the server
 app.listen(PORT, () => {
